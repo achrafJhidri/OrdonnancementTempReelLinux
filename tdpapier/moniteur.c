957673cba24bef7cf27697396814e72da5b5fa64
jhidri.achraf <achrafjh@gmail.com>
2020-04-18 21:56:36 +0200

    semaphore

--- tdpapier/moniteur.c
+++ tdpapier/moniteur.c
@@ -30,3 +30,120 @@ barriere() {
         }
         m.unlock();
 }
+
+
+
+Question 16
+Il a 3 états logiques : MANGE, PENSE, AFAIM.
+
+Question 17
+
+
+mutex m;
+cond file_prive[N];
+EtatPhilo[N] = {PENSE, PENSE, ... , PENSE}; // variables d’état
+
+prendre_fourchettes(i) {
+        m.lock()
+
+        while (EtatPhilo[(i+1)%N] == MANGE ||
+                   EtatPhilo[(i-1+N)%N] == MANGE)
+                file_prive[i].wait(m);
+
+        EtatPhilo[i] = MANGE;
+        m.unlock()
+}
+
+poser_fourchettes(i) {
+        m.lock()
+        EtatPhilo[i] = PENSE;
+        file_prive[(i+1)%N].signal();
+        file_prive[(i-1+N)%N].signal();
+        m.unlock()
+}
+
+Question 18
+
+mutex m;
+cond file_publique;
+EtatPhilo[N] = {PENSE, PENSE, ... , PENSE};
+
+prendre_fourchettes(i) {
+        m.lock();
+        
+        while (EtatPhilo[(i+1)%N] == MANGE ||
+                   EtatPhilo[(i-1+N)%N] == MANGE)
+                file_publique.wait(m);
+
+        EtatPhilo[i] = MANGE;
+
+
+        m.unlock();
+}
+
+poser_fourchettes(i) {
+        m.lock();
+        EtatPhilo[i] = PENSE;
+        file_publique.broadcast();
+        m.unlock();
+}
+
+question 19 
+il se px que quelqun reste en famine 1 2 3 4 5 , 1 4 2 5 , le 3 reste affamé
+
+
+Question 20
+
+sem fourch[N] = {1, 1, 1, ..., 1};
+
+prendre_fourchette(i) {
+        fourch[i].P();
+        fourch[(i+1)%N].P()
+}
+
+poser_fourchette(i) {
+        fourch[i].V();
+        fourch[(i+1)%N].V()
+}
+
+
+Question 21
+
+Il y a un interblocage si tous les philosophes ont pris la fourchette de gauche en même temps !
+
+
+Question 22
+sem fourch[N] = {1, 1, 1, ..., 1};
+
+prendre_fourchette(i) {
+        if(i != N-1) {
+                fourch[i].P();
+                fourch[(i+1)%N].P()
+        }else{
+                fourch[(i+1)%N].P();
+                foruch[i].P();
+        }
+}
+
+poser_fourchette(i) {
+        fourch[i].V();
+        fourch[(i+1)%N].V()
+}
+
+Question 23
+sem fourch[N] = {1, 1, 1, ..., 1};
+sem nbP = N-1
+prendre_fourchette(i) {
+                nbP.P();
+                fourch[i].P();
+                fourch[(i+1)%N].P()
+
+}
+
+poser_fourchette(i) {
+        fourch[i].V();
+        fourch[(i+1)%N].V()
+        nbP.V();
+
+}
+
--- thread/exo4.c
+++ thread/exo4.c
@@ -1,14 +1,15 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-#define N 20   // nb de colonnes
-#define L 1000 // nb de lignes
+#define N 20
+#define L 1000
 int A[N], B[N], RES[N];
 
-sem_t slire;     // pour bloquer lire
-sem_t scalculer; // pour bloquer calculer
+sem_t slire;
+sem_t scalculer[2];
 
 void erreur(char *message) {
     fprintf(stderr, "Erreur: %s\n", message);
@@ -29,14 +30,16 @@ void *lire(void *arg) {
     FILE *fptr = fopen(filename, "r");
 
     for (j=0; j<L; j++) {
-        if (sem_wait(&slire) != 0) erreur("sem_wait");
+        sem_wait(&slire);
+        sem_wait(&slire);
         for (i=0; i<N; i++) {
             if (j%2 == 0)
                 fscanf(fptr, "%d", &A[i]);
             else
                 fscanf(fptr, "%d", &B[i]);
         }
-        if (sem_post(&scalculer) != 0) erreur("sem_post");
+        sem_post(&scalculer[0]);
+        sem_post(&scalculer[1]);
     }
 
     fclose(fptr);
@@ -45,45 +48,53 @@ void *lire(void *arg) {
 
 void *calculer(void *arg) {
     int i,j;
+    int *limits = (int*) arg;
 
-    for (i=0; i<N; i++)
+    for (i=limits[0]; i<limits[1]; i++)
         RES[i] = 0;
 
     for (j=0; j<L; j++) {
-        if (sem_wait(&scalculer) != 0) erreur("sem_wait");
-        for (i=0; i<N; i++) {
+        sem_wait(&scalculer[limits[2]]);
+        for (i=limits[0]; i<limits[1]; i++) {
             if (j%2 == 0)
                 RES[i] = RES[i] + A[i];
             else
                 RES[i] = RES[i] + B[i];
         }
-        if (sem_post(&slire) != 0) erreur("sem_post");
+        sem_post(&slire);
     }
 
     pthread_exit((void*) 0);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) erreur("argc");  // argv[1] = nom du fichier donné par l'utilisateur
+    if (argc != 2) erreur("argc");
 
     void *status;
 
-    if (sem_init(&slire, 0, 1) != 0) erreur("sem_init");
-    if (sem_init(&scalculer, 0, 0) != 0) erreur("sem_init");
+    if (sem_init(&slire, 0, 2) != 0) erreur("sem_init");
+    if (sem_init(&scalculer[0], 0, 0) != 0) erreur("sem_init");
+    if (sem_init(&scalculer[1], 0, 0) != 0) erreur("sem_init");
 
-    pthread_t tid_l, tid_c;
+    pthread_t tid_l, tid_c1, tid_c2;
     if (pthread_create(&tid_l, NULL, lire, (void*) argv[1]) != 0) erreur("pthread_create");
-    if (pthread_create(&tid_c, NULL, calculer, NULL) != 0) erreur("pthread_create");
+    int limits1[2] = {0, N/2};
+    if (pthread_create(&tid_c1, NULL, calculer, (void*) limits1) != 0) erreur("pthread_create");
+    int limits2[2] = {N/2, N};
+    if (pthread_create(&tid_c2, NULL, calculer, (void*) limits2) != 0) erreur("pthread_create");
 
     pthread_join(tid_l, &status);
     if (status != 0) erreur("pthread_join");
-    pthread_join(tid_c, &status);
+    pthread_join(tid_c1, &status);
+    if (status != 0) erreur("pthread_join");
+    pthread_join(tid_c2, &status);
     if (status != 0) erreur("pthread_join");
 
     print_array(RES);
 
     if (sem_destroy(&slire) != 0) erreur("sem_destroy");
-    if (sem_destroy(&scalculer) != 0) erreur("sem_destroy");
+    if (sem_destroy(&scalculer[0]) != 0) erreur("sem_destroy");
+    if (sem_destroy(&scalculer[1]) != 0) erreur("sem_destroy");
 
     return 0;
 }
